59a5b7ddd2148083c1656bbfe61b517df6277fa1
Chris <christopherflinn@hotmail.com>
2019-03-22 11:48:31 +0000

    Basic functionality. Menus, maven and database

--- src/main/java/PLATFORM/AuctionSystem.java
+++ src/main/java/PLATFORM/AuctionSystem.java
@@ -1,33 +1,37 @@
 package PLATFORM;
 
 import CORE.Auction;
-import CORE.User;
 
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class AuctionSystem {
-    private Integer userID = null;
     private final Scanner scanner = new Scanner(System.in);
     private final DatabaseAccess databaseAccess;
-
     List<Auction> auctionList = null; //Used to store last updated auction information
+    private Integer userID = null;
 
-    public AuctionSystem() throws SQLException {
+    public AuctionSystem() {
         databaseAccess = new DatabaseAccess();
     }
 
-    public void begin() throws SQLException {
+    public void begin() {
         Integer input = null;
-        int[] counts = databaseAccess.GetSystemStats();
-        System.out.printf("Welcome to the Auction System. \n " +
-                        "Stats: Users %d, Total Auctions %d, Active Auctions %d, Bids %d",
-                counts[0], counts[1], counts[2], counts[3]);
+        try {
+            int[] counts = databaseAccess.GetSystemStats();
+            System.out.printf("Welcome to the Auction System. \n " +
+                            "Stats: Users %d, Total Auctions %d, Active Auctions %d, Bids %d",
+                    counts[0], counts[1], counts[2], counts[3]);
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.toString());
+            return;
+        }
 
-        if (userID != null) {
-            do {
+        do {
+            if (userID == null) {
                 System.out.println("Please select an option:");
                 System.out.println("[1] Create Account");
                 System.out.println("[2] Login");
@@ -35,7 +39,10 @@ public class AuctionSystem {
                 System.out.println("[0] Exit");
                 try {
                     input = scanner.nextInt();
-                    switch(input){
+                    if (scanner.hasNextLine()) {
+                        scanner.nextLine();
+                    }
+                    switch (input) {
                         case 1:
                             CreateAccount();
                             break;
@@ -49,21 +56,31 @@ public class AuctionSystem {
                             System.out.println("test");
                     }
                 } catch (InputMismatchException e) {
-                    scanner.next();
+                    if (scanner.hasNextLine()) {
+                        scanner.nextLine();
+                    }
                     System.out.println("That is not a valid input");
                 }
+            } else {
+                try {
+                    DatabaseAccess.UserStat userStat = databaseAccess.GetUserStats();
+                    System.out.printf("Logged in as %s. You have %d Auctions active and %d Bids open", userStat.username,
+                            userStat.auctions, userStat.bid);
+                }catch (SQLException e) {
+                    System.out.println("Error: " + e.toString());
+                    return;
+                }
 
-            }while(input != 0);
-        } else {
-            do {
-                System.out.println("Logged in as %s. You have %d Auctions active and %d Bids open");
                 System.out.println("Please select an option:");
                 System.out.println("[1] Create Auction");
                 System.out.println("[2] Browse Auctions");
                 System.out.println("[0] Exit");
                 try {
                     input = scanner.nextInt();
-                    switch(input){
+                    if (scanner.hasNextLine()) {
+                        scanner.nextLine();
+                    }
+                    switch (input) {
                         case 1:
                             CreateAuction();
                             break;
@@ -74,38 +91,79 @@ public class AuctionSystem {
                             System.out.println("test");
                     }
                 } catch (InputMismatchException e) {
-                    scanner.next();
+                    if (scanner.hasNextLine()) {
+                        scanner.nextLine();
+                    }
                     System.out.println("That is not a valid input");
                 }
-
-            }while(input != 0);
-        }
-
-
-
+            }
+        }while(input != 0);
     }
 
+    //TODO Allow exit to menu
     public void CreateAccount() {
         int complete = 0;
         do {
             System.out.println("Please enter your chosen username");
             String username = scanner.nextLine();
-            if (!databaseAccess.CheckAvailability(username)) {
-                System.out.println("That username is already taken, please try another");
+            System.out.println(username);
+            try {
+                if (!databaseAccess.CheckAvailability(username)) {
+                    System.out.println("That username is already taken, please try another");
+                    continue;
+                }
+            } catch (SQLException e) {
+                System.out.println("Error: " + e.toString());
                 continue;
             }
+
             System.out.println("Please enter your chosen password");
             String password = scanner.nextLine();
-            if (databaseAccess.CreateUser(username, password)) {
-                System.out.println("Account successfully created! Please login.");
-                complete = 1;
-            } else {
-                System.out.println("Could not create user, please try again");
+            try {
+                if (databaseAccess.CreateUser(username, password)) {
+                    System.out.println("Account successfully created! Please login.");
+                    complete = 1;
+                } else {
+                    System.out.println("Could not create user, please try again");
+                }
+            } catch (NoSuchAlgorithmException | SQLException e) {//TODO Maybe change to catching generic exceptions instead of specific ones, while we done make use of specificity anyway
+                System.out.println("Error: " + e.toString());
+                continue;
             }
-        }while(complete !=1);
+
+        } while (complete != 1);
     }
 
     public void Login() {
+       Login: do {
+            System.out.println("Please enter username");
+            String username = scanner.nextLine();
+            System.out.println("Please enter your password");
+            String password = scanner.nextLine();
+            try {
+                userID = databaseAccess.LoginUser(username, password);
+            }catch (NoSuchAlgorithmException | SQLException e) {
+                System.out.println("Error: " + e.toString());
+                continue;
+            }
+            if (userID == null) {
+                System.out.println("Username or password incorrect. Please choose and option:");
+                System.out.println("[1] Retry");
+                System.out.println("[0] Return");
+                Integer input = scanner.nextInt();
+                if (scanner.hasNextLine()) {
+                    scanner.nextLine();
+                }
+                switch (input) {
+                    case 1:
+                        continue;
+                    case 0:
+                        break Login;
+                    default:
+                        break Login;
+                }
+            }
+        } while (userID == null);
 
     }
 
--- src/main/java/PLATFORM/DatabaseAccess.java
+++ src/main/java/PLATFORM/DatabaseAccess.java
@@ -1,24 +1,23 @@
 package PLATFORM;
 
-import CORE.Bid;
 import CORE.Auction;
+import CORE.Bid;
 import CORE.Item;
 import CORE.User;
-
-
 import com.mysql.cj.jdbc.MysqlDataSource;
-
-import java.util.Base64;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Base64;
 
 
 public final class DatabaseAccess {
     private Connection connection;
     private MysqlDataSource dataSource = new MysqlDataSource();
-
 
 
     public DatabaseAccess() {
@@ -29,56 +28,94 @@ public final class DatabaseAccess {
 
     }
 
-    public boolean CheckAvailability(String username) {
-        Connection con = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try {
-            con = dataSource.getConnection();
-            ps = con.prepareStatement("SELECT ? FROM user");
-            ps.setString(1, username);
-            rs = ps.executeQuery();
-            if (rs.next()) {
-                return false;
-            } else {
-                return true;
-            }
-        } catch (SQLException ex) {
-            System.out.println("Error checking name availability: " + ex.toString());
-        } finally {
-            CloseQueryItems(con, ps, rs);
-        }
-        return false;
-    }
-
-    public int[] GetSystemStats() {
-        Connection con = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try {
-            con = dataSource.getConnection();
-            ps = con.prepareStatement("SELECT(SELECT COUNT(*) FROM user) AS c1,(SELECT COUNT(*) FROM auction) AS c2," +
-                    " (SELECT COUNT(*) FROM auction WHERE status = 'ACTIVE') AS c3, (SELECT COUNT(*) FROM bid) AS c4;");
-            rs = ps.executeQuery();
-            int counts[] = new int[4];
-            rs.first();
-            counts[0] = rs.getInt("c1");
-            counts[1] = rs.getInt("c2");
-            counts[2] = rs.getInt("c3");
-            counts[3] = rs.getInt("c4");
-            return counts;
-        } catch (SQLException ex) {
-            System.out.println("Error getting stats: " + ex.toString());
-            return null;
-        } finally {
-            CloseQueryItems(con, ps, rs);
-        }
-    }
-
     private static void CloseQueryItems(Connection con, PreparedStatement ps, ResultSet rs) {
-        try { rs.close(); } catch (Exception e) {}
-        try { ps.close(); } catch (Exception e) {}
-        try { con.close(); } catch (Exception e) {}
+        try {
+            rs.close();
+        } catch (Exception e) {
+        }
+        try {
+            ps.close();
+        } catch (Exception e) {
+        }
+        try {
+            con.close();
+        } catch (Exception e) {
+        }
+    }
+
+    private static void CloseQueryItems(Connection con, PreparedStatement ps) {
+        try {
+            ps.close();
+        } catch (Exception e) {
+        }
+        try {
+            con.close();
+        } catch (Exception e) {
+        }
+    }
+
+    public boolean CheckAvailability(String username) throws SQLException {
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        con = dataSource.getConnection();
+        ps = con.prepareStatement("SELECT ? FROM user");
+        ps.setString(1, username);
+        rs = ps.executeQuery();
+        if (rs.next()) {
+            CloseQueryItems(con, ps, rs);
+            return false;
+        } else {
+            CloseQueryItems(con, ps, rs);
+            return true;
+        }
+
+    }
+
+    public int[] GetSystemStats() throws SQLException {
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        con = dataSource.getConnection();
+        ps = con.prepareStatement("SELECT(SELECT COUNT(*) FROM user) AS c1,(SELECT COUNT(*) FROM auction) AS c2," +
+                " (SELECT COUNT(*) FROM auction WHERE status = 'ACTIVE') AS c3, (SELECT COUNT(*) FROM bid) AS c4;");
+        rs = ps.executeQuery();
+        int counts[] = new int[4];
+        rs.first();
+        counts[0] = rs.getInt("c1");
+        counts[1] = rs.getInt("c2");
+        counts[2] = rs.getInt("c3");
+        counts[3] = rs.getInt("c4");
+        CloseQueryItems(con, ps, rs);
+        return counts;
+
+    }
+
+    public UserStat GetUserStats(int userID) throws SQLException {
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        con = dataSource.getConnection();
+        ps = con.prepareStatement("SELECT(SELECT userName FROM user WHERE userID = ?) as c1, (SELECT COUNT(*) FROM auction WHERE userID = ?) as c2," +
+                "(SELECT COUNT(*) FROM bid INNER JOIN bid_auction ba on bid.bidID = ba.bidID INNER JOIN auction a on" +
+                " ba.auctionID = a.auctionID AND status = 'ACTIVE' INNER JOIN user_bid ub on bid.bidID = ub.bidID AND ub.userID = ?) as c3;");
+        ps.setInt(1,userID);
+        ps.setInt(2,userID);
+        ps.setInt(3,userID);
+        rs = ps.executeQuery();
+        return  new UserStat(rs.getString("c1"), rs.getInt("c2"), rs.getInt("c3"));
+    }
+
+    public class UserStat {
+        public UserStat(String username, int auctions, int bid) {
+            this.username = username;
+            this.auctions = auctions;
+            this.bid = bid;
+        }
+        String username;
+        int auctions;
+        int bid;
     }
 
     public ResultSet InsertRecord(Bid bid) {
@@ -100,24 +137,45 @@ public final class DatabaseAccess {
         return encoded;
     }
 
-    public boolean CreateUser(String username, String password){
+    public boolean CreateUser(String username, String password) throws SQLException, NoSuchAlgorithmException {
+        Connection con = null;
+        PreparedStatement ps = null;
+        Integer executeReturn = null;
+
+        con = dataSource.getConnection();
+        ps = con.prepareStatement("INSERT INTO user(userName, password) VALUES (?,?)");
+        ps.setString(1, username);
+        ps.setString(2, HashPassword(password));
+        executeReturn = ps.executeUpdate();
+        CloseQueryItems(con, ps);
+        if (executeReturn == 1) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public Integer LoginUser(String username, String password) throws SQLException, NoSuchAlgorithmException {
         Connection con = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        try {
-            con = dataSource.getConnection();
-            ps = con.prepareStatement("INSERT INTO user(userName, password) VALUES (?,?)");
-            ps.setString(1,username);
-            ps.setString(2,HashPassword(password));
-            rs = ps.executeQuery();
-            return true;
-        } catch (NoSuchAlgorithmException | SQLException ex) {
-            System.out.println("Error creating user: " + ex.toString());
-            return false;
-        } finally {
+        con = dataSource.getConnection();
+        ps = con.prepareStatement("SELECT userID FROM user WHERE userName = ? AND password = ?");
+        ps.setString(1, username);
+        ps.setString(2, HashPassword(password));
+        rs = ps.executeQuery();
+        if (rs.next()) {
+            int rt = rs.getInt(1);
             CloseQueryItems(con, ps, rs);
+            return rt;
+        } else {
+            CloseQueryItems(con, ps, rs);
+            return null;
         }
+
     }
+
 
     public ResultSet UpdateRecord(Bid bid) {
         return null;
@@ -134,7 +192,6 @@ public final class DatabaseAccess {
     public ResultSet UpdateRecord(User user) {
         return null;
     }
-
 
 
 }
